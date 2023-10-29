236fdd56e5d5aa8bb095432330abdc8d4f0f9fb9
Syrtsevnikita <145428432+SyrNik24@users.noreply.github.com>
2023-10-29 22:08:54 +0300

    Add files via upload

---
+++ consti.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	//Начало кода
+
+	//float
+	cout << "FLOAT"<<endl;
+	const float FL_NAME1 = 1.1;
+	cout << "Float 1=" << FL_NAME1 << endl;
+	const float FL_NAME2 = 1.2;
+	cout << "Float 2=" << FL_NAME2 << endl;
+	const float FL_NAME3 = 1.3;
+	cout << "Float 3=" << FL_NAME3 << endl;
+	const float FL_NAME4 = 1.4;
+	cout << "Float 1=" << FL_NAME4 << endl;
+	const float FL_NAME5 = 1.5;
+	cout << "Float 1=" << FL_NAME5 << endl;
+	const float FL_NAME6 = 1.6;
+	cout << "Float 1=" << FL_NAME6 << endl;
+	const float FL_NAME7 = 1.7;
+	cout << "Float 1=" << FL_NAME7 << endl;
+	const float FL_NAME8 = 1.8;
+	cout << "Float 1=" << FL_NAME8 << endl;
+	const float FL_NAME9 = 1.9;
+	cout << "Float 1=" << FL_NAME9 << endl;
+	const float FL_NAME10 = 1.11;
+	cout << "Float 1=" << FL_NAME10 << endl;
+
+	//double
+	cout << "DOUBLE" << endl;
+	const double DB_NAME1 = 1.1;
+	cout << "Double 1=" << DB_NAME1 << endl;
+	const double DB_NAME2 = 1.2;
+	cout << "Double 1=" << DB_NAME2 << endl;
+	const double DB_NAME3 = 1.3;
+	cout << "Double 1=" << DB_NAME3 << endl;
+	const double DB_NAME4 = 1.4;
+	cout << "Double 1=" << DB_NAME4 << endl;
+	const double DB_NAME5 = 1.5;
+	cout << "Double 1=" << DB_NAME5 << endl;
+	const double DB_NAME6 = 1.6;
+	cout << "Double 1=" << DB_NAME6 << endl;
+	const double DB_NAME7 = 1.7;
+	cout << "Double 1=" << DB_NAME7 << endl;
+	const double DB_NAME8 = 1.8;
+	cout << "Double 1=" << DB_NAME8 << endl;
+	const double DB_NAME9 = 1.9;
+	cout << "Double 1=" << DB_NAME9 << endl;
+	const double DB_NAME10 = 1.11;
+	cout << "Double 1=" << DB_NAME10 << endl;
+
+	//int
+	cout << "INT" << endl;
+	const int IN_NAME1 = 1;
+	cout << "Int 1=" << IN_NAME1 << endl;
+	const int IN_NAME2 = 2;
+	cout << "Int 2=" << IN_NAME2 << endl;
+	const int IN_NAME3 = 3;
+	cout << "Int 3=" << IN_NAME3 << endl;
+	const int IN_NAME4 = 4;
+	cout << "Int 4=" << IN_NAME4 << endl;
+	const int IN_NAME5 = 5;
+	cout << "Int 5=" << IN_NAME5 << endl;
+	const int IN_NAME6 = 6;
+	cout << "Int 6=" << IN_NAME6 << endl;
+	const int IN_NAME7 = 7;
+	cout << "Int 7=" << IN_NAME7 << endl;
+	const int IN_NAME8 = 8;
+	cout << "Int 8=" << IN_NAME8 << endl;
+	const int IN_NAME9 = 9;
+	cout << "Int 9=" << IN_NAME9 << endl;
+	const int IN_NAME10 = 10;
+	cout << "Int 10=" << IN_NAME10 << endl;
+
+	//short
+	cout << "SHORT" << endl;
+	const short SH_NAME1 = 1;
+	cout << "Short 1=" << SH_NAME1 << endl;
+	const short SH_NAME2 = 2;
+	cout << "Short 2=" << SH_NAME2 << endl;
+	const short SH_NAME3 = 3;
+	cout << "Short 3=" << SH_NAME3 << endl;
+	const short SH_NAME4 = 4;
+	cout << "Short 4=" << SH_NAME4 << endl;
+	const short SH_NAME5 = 5;
+	cout << "Short 5=" << SH_NAME5 << endl;
+	const short SH_NAME6 = 6;
+	cout << "Short 6=" << SH_NAME6 << endl;
+	const short SH_NAME7 = 7;
+	cout << "Short 7=" << SH_NAME7 << endl;
+	const short SH_NAME8 = 8;
+	cout << "Short 8=" << SH_NAME8 << endl;
+	const short SH_NAME9 = 9;
+	cout << "Short 9=" << SH_NAME9 << endl;
+	const short SH_NAME10 = 10;
+	cout << "Short 10=" << SH_NAME10 << endl;
+
+	//long
+	cout << "LONG" << endl;
+	const long LG_NAME1 = 1;
+	cout << "Long 1=" << LG_NAME1<<endl;
+	const long LG_NAME2 = 2;
+	cout << "Long 2=" << LG_NAME2 << endl;
+	const long LG_NAME3 = 3;
+	cout << "Long 3=" << LG_NAME3 << endl;
+	const long LG_NAME4 = 4;
+	cout << "Long 4=" << LG_NAME4 << endl;
+	const long LG_NAME5 = 5;
+	cout << "Long 5=" << LG_NAME5 << endl;
+	const long LG_NAME6 = 6;
+	cout << "Long 6=" << LG_NAME6 << endl;
+	const long LG_NAME7 = 7;
+	cout << "Long 7=" << LG_NAME7 << endl;
+	const long LG_NAME8 = 8;
+	cout << "Long 8=" << LG_NAME8 << endl;
+	const long LG_NAME9 = 9;
+	cout << "Long 9=" << LG_NAME9 << endl;
+	const long LG_NAME10 = 10;
+	cout << "Long 10=" << LG_NAME10 << endl;
+
+	//char
+	cout << "CHAR" << endl;
+	const char CH_NAME1 = 'a';
+	cout << "Char 1=" << CH_NAME1 << endl;
+    const char CH_NAME2 = 'b';
+	cout << "Char 2=" << CH_NAME2 << endl;
+	const char CH_NAME3 = 'c';
+	cout << "Char 3=" << CH_NAME3 << endl;
+	const char CH_NAME4 = 'd';
+	cout << "Char 4=" << CH_NAME4 << endl;
+	const char CH_NAME5 = 'e';
+	cout << "Char 5=" << CH_NAME5 << endl;
+	const char CH_NAME6 = 'f';
+	cout << "Char 6=" << CH_NAME6 << endl;
+	const char CH_NAME7 = 'g';
+	cout << "Char 7=" << CH_NAME7 << endl;
+	const char CH_NAME8 = 'h';
+	cout << "Char 8=" << CH_NAME8 << endl;
+	const char CH_NAME9 = 'i';
+	cout << "Char 9=" << CH_NAME9 << endl;
+	const char CH_NAME10 = 'j';
+	cout << "Char 10=" << CH_NAME10 << endl;
+
+	//bool
+	cout << "BOOl" << endl;
+	const bool BL_NAME1 = true;
+	cout << "Bool 1="<< BL_NAME1 << endl;
+	const bool BL_NAME2 = true;
+	cout << "Bool 2=" << BL_NAME2 << endl;
+	const bool BL_NAME3 = true;
+	cout << "Bool 3=" << BL_NAME3 << endl;
+	const bool BL_NAME4 = true;
+	cout << "Bool 4=" << BL_NAME4 << endl;
+	const bool BL_NAME5 = true;
+	cout << "Bool 5=" << BL_NAME5 << endl;
+	const bool BL_NAME6 = false;
+	cout << "Bool 6=" << BL_NAME6 << endl;
+	const bool BL_NAME7 = false;
+	cout << "Bool 7=" << BL_NAME7 << endl;
+	const bool BL_NAME8 = false;
+	cout << "Bool 8=" << BL_NAME8 << endl;
+	const bool BL_NAME9 = false;
+	cout << "Bool 9=" << BL_NAME9 << endl;
+	const bool BL_NAME10 = false;
+	cout << "Bool 10=" << BL_NAME10 << endl;
+	return 0;
+}
